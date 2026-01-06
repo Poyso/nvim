291594423d0027a73f7e1b89b7344b2335045712
Poyso <costantini039@gmail.com>
2026-01-06 14:49:41 +0100

    added colorscheme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'RedsXDD/neopywal.nvim',
+    name = 'neopywal',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+}
